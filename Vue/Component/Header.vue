4fbd6eb2d522e8e83dea39ac47b3af545b83270e
Tenzin Nyima <tnyima06@gmail.com>
2021-04-19 15:32:51 -0400

    Here are the vue files

---
+++ Vue/Component/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>{{ title }}</h1>
+  <Button text = "Breed" color = "green"/>
+</template>
+
+<script>
+import Button from "./Button"
+  export default {
+  name: "Header",
+    props: {
+    title: String,
+    },
+    components:{
+    Button
+    }
+}
+</script>
+
+<style scoped>
+header{
+  display:flex;
+  justify-content: space-between;
+   align-items: center;
+   margin-bottom: 20px;
+}
+</style>
+
+
