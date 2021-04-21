d539e1c70c7c8afef72b5f8045f6fe6cd8e7b8b3
Anthony6197 <50277615+Anthony6197@users.noreply.github.com>
2021-04-21 21:19:05 +0800

    Add files via upload
    Adding prototype of vue components used to represents plant in the inventory, not have all functions implemented

---
+++ Vue/Component/Plant.vue
@@ -0,0 +1,39 @@
+<template>
+    <div @dblclick="$emit('toggle-reminder',plant.id)" :class = 
+    "[plant.reminder ? 'reminder' : '', 'plant']">
+        <h3>{{ plant.text }}
+            <i @click="$emit('delete-plant',plant.id)" class="fas fa-times"></i>
+        </h3>
+        <p>{{ plant.description }}</p>
+    </div>
+    
+</template>
+
+<script>
+export default{
+    name : 'Plant',
+    props:{
+        plant:Object
+    },
+}
+</script>
+<style>
+.fas {
+  color: red;
+}
+.plant {
+  background: #f4f4f4;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+.plant.reminder {
+  border-left: 5px solid green;
+}
+.plant h3 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+</style>
---
+++ Vue/Component/Plants.vue
@@ -0,0 +1,20 @@
+<template>
+    <div :key = "plant.id" v-for = "plant in plants">
+        <Plant @delete-plant="$emit('delete-plant',plant.id)" 
+        :plant = "plant"/>
+    </div>
+</template>
+
+<script>
+import Plant from "./Plant"
+export default{
+    name: 'Plants',
+    props: {
+        plants: Array
+    },
+    components: {
+        Plant
+    },
+    emits: ['delete-plant'],
+}
+</script>
