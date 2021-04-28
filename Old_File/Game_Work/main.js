53f2ff93468627109d4829b0c61a137b5a09e909
leendiab <leenoo.diab@gmail.com>
2021-04-28 18:35:12 -0400

    updated random function + aesthetic changes

--- Old_File/Game_Work/main.js
+++ Old_File/Game_Work/main.js
@@ -2,11 +2,15 @@
 function preload() {
     sunflower = loadImage("sunflower.png");
     raspberry = loadImage("raspberry.png");
-    corpseFlower = loadImage("flower-transparent1.png")
+    corpseFlower = loadImage("flower-transparent1.png");
+    basicLeaf = loadImage("leaf.png");
+    pear = loadImage("pear.png");
+    blueberry = loadImage("blueberry.png")
 }
 function setup() {
-    createCanvas(600, 600);
-    background(174, 217, 165);
+    createCanvas(screen.width, screen.height*0.75);
+    // background(174, 217, 165);
+    background(154, 193, 208);
     strokeWeight(3);
 
     let testPlant1 = {
@@ -17,10 +21,12 @@ function setup() {
         rightBranchSize : .42,
         rightBranchAngle : Math.PI/9,
         midBranchSize : .8,
-        flowerProb : 0.06,
+        flowerProb : 0.01,
         flower : sunflower,
-        berryProb: 0,
-        berry : null
+        berryProb: 0.1,
+        berry : blueberry,
+        leafProb : 0.2,
+        leaf: basicLeaf
     }
 
     let testPlant2 = {
@@ -31,15 +37,23 @@ function setup() {
         rightBranchSize : .65,
         rightBranchAngle : Math.PI/7,
         midBranchSize : .6,
-        flowerProb : 0.2,
+        flowerProb : 0.01,
         flower : corpseFlower,
         berryProb : 0.2,
-        berry : raspberry
+        berry : raspberry,
+        leafProb : 0,
+        leaf: null
     }
 
     childPlant = breedPlant(testPlant1, testPlant2);
-    draw(childPlant, 280, 580, childPlant.initialSize, Math.PI * 1.5, 1, 8)
-        // draw(280, 580, 40, Math.PI * 1.5, 1, 2)
+    //draw(testPlant1,(screen.width)/2, screen.height, testPlant1.initialSize, Math.PI * 1.5, 1, 8)
+    draw(childPlant,(screen.width)/2, screen.height*.75, childPlant.initialSize, Math.PI * 1.5, 1, 8)
+    //draw(testPlant2,(screen.width)/2, screen.height, testPlant2.initialSize, Math.PI * 1.5, 1, 8)
+
+    //draw(testPlant1, 280, 580, testPlant1.initialSize, Math.PI * 1.5, 1, 8)
+    //draw(testPlant2, 280, 580, testPlant2.initialSize, Math.PI * 1.5, 1, 8)
+
+    // draw(280, 580, 40, Math.PI * 1.5, 1, 2)
 }
 
 function draw(plant, x, y, size, theta, branchDirection, levels) {
@@ -56,7 +70,7 @@ function draw(plant, x, y, size, theta, branchDirection, levels) {
     if (1 <= levels < 2) { //&& !(279 < centerX < 281)
         let r = Math.random();
         if (r < 0.1) {
-            if (r < plant.flowerProb) {
+            if (r < plant.flowerProb && plant.flower != null) {
                 push();
                 translate(centerX, centerY);
                 rotate(theta + PI / 2);
@@ -71,6 +85,7 @@ function draw(plant, x, y, size, theta, branchDirection, levels) {
                 rotate(theta + PI / 2);
                 image(plant.berry, 0, 0, 20, 20);
                 pop();
+                //drawElement(plant, centerX, centerY, plant.berry);
             }
         }
     }
@@ -82,6 +97,14 @@ function draw(plant, x, y, size, theta, branchDirection, levels) {
     draw(plant, x2, y2, size * plant.leftBranchSize, theta + branchDirection * plant.rightBranchAngle, branchDirection, levels - 1);
     draw(plant, x2, y2, size * plant.rightBranchSize, theta - branchDirection * plant.leftBranchAngle, branchDirection, levels - 1);
     draw(plant, x2, y2, size * plant.midBranchSize, theta, -branchDirection, levels - 1);
+}
+
+function drawElement (plant, x, y, theta, element){
+    push();
+    translate(centerX, centerY);
+    rotate(theta + PI / 2);
+    image(element, 0, 0, 20, 20);
+    pop();
 }
 
 function breedPlant(plant1, plant2){
@@ -96,17 +119,15 @@ function breedPlant(plant1, plant2){
         flowerProb : randomInRange(plant1.flowerProb, plant2.flowerProb),
         flower : randomChoice(plant1.flower, plant2.flower),
         berryProb : randomInRange(plant1.berryProb, plant2.berryProb),
-        berry : randomChoice(plant1.berry, plant2.berry)
+        berry : randomChoice(plant1.berry, plant2.berry),
+        leafProb : randomInRange(plant1.leafProb, plant2.leafProb),
+        leaf: randomChoice(plant1.leaf, plant2.leaf)
     }
 }
 
 function randomInRange(value1, value2){
-    if (value1 > value2){
-        return Math.random() * (value1 - value2) + value2;
-    }
-    else{
-        return Math.random() * (value2 - value1) + value1;
-    }
+    return Math.random() * Math.abs(value1 - value2) + Math.min(value1, value2);
+
 }
 
 function randomChoice(value1, value2){
